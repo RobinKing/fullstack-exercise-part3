9d719d63dcfd51e3208f7353b30e81982c37a9b5
robinking <jingjl1001@gmail.com>
2021-12-12 21:30:38 +0800

    After step 6. and lint code.
    Signed-off-by: robinking <jingjl1001@gmail.com>

--- index.js
+++ index.js
@@ -5,79 +5,90 @@ const app = express()
 app.use(express.json())
 
 let persons = [
-    {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
+  {
+    "id": 1,
+    "name": "Arto Hellas",
+    "number": "040-123456"
+  },
+  {
+    "id": 2,
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523"
+  },
+  {
+    "id": 3,
+    "name": "Dan Abramov",
+    "number": "12-43-234345"
+  },
+  {
+    "id": 4,
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122"
+  }
 ]
 
 app.get('/api/persons', (req, res) => {
-    res.json(persons)
+  res.json(persons)
 })
 
 app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const person = persons.find(person => person.id === id)
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
 
-    if (person) {
-        res.json(person)
-    } else {
-        res.status(404).end()
-    }
-    console.log(person)
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+  console.log(person)
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter(p => p.id !== id)
+  const id = Number(req.params.id)
+  persons = persons.filter(p => p.id !== id)
 
-    res.status(204).end()
+  res.status(204).end()
 })
 
 app.post('/api/persons', (req, res) => {
-    const person = req.body
-    let id = Math.floor(Math.random() * 10000000000)
-    for (triedTime = 0; triedTime < 10; triedTime++)
-    {
-        if (persons.find( p => p.id === id))
-        {
-            id = Math.floor(Math.random() * 10000000000000)
-        }
-        else
-        {
-            break;
-        }
+  const person = req.body
+
+  if ((!person.name) || (!person.number)) {
+    return res.status(400).json({
+      error: 'name or number missing'
+    })
+  }
+
+  if (persons.find(p => p.name === person.name)) {
+    return res.status(400).json({
+      error: 'name must be unique'
+    })
+  }
+
+  let id = Math.floor(Math.random() * 10000000000)
+  for (triedTime = 0; triedTime < 10; triedTime++) {
+    if (persons.find(p => p.id === id)) {
+      id = Math.floor(Math.random() * 10000000000000)
     }
-    person.id = id
-    persons = persons.concat(person)
-    res.json(person)
-    console.log(person)
+    else {
+      break;
+    }
+  }
+
+  person.id = id
+  persons = persons.concat(person)
+  res.json(person)
+  console.log(person)
 })
 
 app.get('/info', (req, res) => {
-    const info = `<p>Phonebook has info for ${persons.length} people</p>
+  const info = `<p>Phonebook has info for ${persons.length} people</p>
     <p>${Date()}</p>`
-    res.send(info)
-    console.log('get /info')
+  res.send(info)
+  console.log('get /info')
 })
 
 const PORT = 3001
 app.listen(PORT, () => {
-    console.log(`start server on ${PORT}`)
+  console.log(`start server on ${PORT}`)
 })
